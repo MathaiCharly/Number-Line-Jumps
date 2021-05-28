fcfe97895b698f4d2182ce722a8475844a1401d7
MathaiCharly <79537013+MathaiCharly@users.noreply.github.com>
2021-05-29 00:28:47 +0530

    Add files via upload

---
+++ LineNumberJump.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    long s1=x1;
+    long s2=x2;
+    int i;
+    String s;
+    s="p";
+    if(v2>=v1)
+    {
+        s="NO";
+    }
+    else if(v1>v2)
+    {
+        for(i=1;i>0;i++)
+        {
+            s1=x1+(i*v1);
+            s2=x2+(i*v2);
+            if(s1==s2)
+            {
+                s="YES";
+                break;
+            }
+            if(s1>s2)
+            {
+                s="NO";
+                break;
+            }  
+        }
+    }
+    return s;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+        int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+        String result = Result.kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
